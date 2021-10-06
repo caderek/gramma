daf456883c92e1ceb750ffeb1e2d9a0f1c140908
Maciej Caderek <maciej.caderek@gmail.com>
2021-10-06 23:19:59 +0200

    Hook test

--- src/commands/hook.js
+++ src/commands/hook.js
@@ -3,7 +3,7 @@ const fs = require("fs")
 const checkInteractively = require("../actions/checkInteractively")
 const saveNow = require("../actions/saveNow")
 
-const hookCode = `hook code 2`
+const hookCode = `hook code 3`
 
 console.log(hookCode)
 
